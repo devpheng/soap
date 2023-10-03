b15eb60ccad342b2614ee4ef82a48e879610f157
pheng <pheng@marinax.co.jp>
2023-10-03 13:46:50 +0700

    Create AuthenticationController for register and login

---
+++ src/main/java/com/api/api/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.api.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.api.model.LoginResponseDTO;
+import com.api.api.model.RegistrationDTO;
+import com.api.api.model.User;
+import com.api.api.service.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+     @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getFirstName(), body.getLastName(), body.getEmail(), body.getUsername(), body.getPassword());
+    }
+    
+    @PostMapping("/login")
+    public LoginResponseDTO loginUser(@RequestBody RegistrationDTO body){
+        return authenticationService.loginUser(body.getUsername(), body.getPassword());
+    }
+}
